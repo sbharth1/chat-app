5fb5a6f4a8bbb95630cab627293165ecf3d75cae
Siddharth <165468666+sbharth1@users.noreply.github.com>
2025-01-05 03:52:24 +0530

    add navbar content

--- client/src/pages/Navbar.tsx
+++ client/src/pages/Navbar.tsx
@@ -2,6 +2,14 @@ import { useEffect } from "react"
 import Cookies from "js-cookie"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 const Navbar = () => {
   const navigate = useNavigate()
@@ -25,8 +33,29 @@ const Navbar = () => {
     }
     fetchUser()
   },[])
-  return (
-    <h1>Navbar</h1>
+  return (<>
+  
+  <Box sx={{ flexGrow: 1}}>
+      <AppBar position="static" sx={{background:"black"}}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Navbar
+          </Typography>
+          <Button color="inherit">User</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  
+  </>
   )
 }
 
