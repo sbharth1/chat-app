f9b853c0fd317923aa17d68394c637683050e000
Siddharth <165468666+sbharth1@users.noreply.github.com>
2025-01-05 22:36:32 +0530

    add user page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,14 +5,19 @@ import Signup from './components/Signup.tsx'
 import Login from './components/Login.tsx'
 import Navbar from './pages/Navbar.tsx'
 import NotFound from './pages/NotFound.tsx'
+import UserProfile from './pages/UserProfile.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <Router>
     <Routes>
+      {/* public routes */}
       <Route path="*" element={<NotFound/>}></Route>
       <Route path="/api/login" element={<Login/>}></Route>
       <Route path="/api/signup" element={<Signup/>}></Route>
+
+      {/* private routes  */}
       <Route path="/api/dashboard" element={<Navbar/>}></Route>
+      <Route path="/api/user" element={<UserProfile/>}></Route>
       </Routes> 
   </Router>
 )
